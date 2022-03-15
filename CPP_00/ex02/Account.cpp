1a717184a17f2e3c0a64f851f82b2207d89c1a48
yavuzsonmez <contact@yavuzsonmez.com>
2022-03-15 16:05:58 +0100

    updaate

--- CPP_00/ex02/Account.cpp
+++ CPP_00/ex02/Account.cpp
@@ -1,39 +1,50 @@
-t::_nbts = 0;
-t::_totalAmount = 0;
-t::_totalNbDeposits = 0;
-t::_totalNbWithdrawals = 0;
+#include "Account.hpp"
+#include <iostream>
+
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
 
 
-t::t(void) {
-
-}
-
-t::~t(void) {
-
-}
-
-int t::getNbts(void) {
-	return (t::_nbts);
-}
-int t::getTotalAmount(void) {
-	return (t::_totalAmount);
-}
-int t::getNbDeposits(void) {
-	return (t::_totalNbDeposits);
-}
-int t::getNbWithdrawals(void) {
-	return (t::_totalNbWithdrawals);
-}
-
-void t::displaytsInfos(void) {
+Account::Account(void) {
 
 }
 
-int t::checkAmount (void) const {
+Account::Account(int initial_deposit) : _amount(initial_deposit) {
+	_displayTimestamp();
+	this->_accountIndex = _nbAccounts;
+	std::cout << " index:" << this->_accountIndex << ";amount:" << this->_amount << ";created" << std::endl;
+	_nbAccounts += 1;
+	_totalAmount += this->_amount;
+}
+
+Account::~Account(void) {
+
+}
+
+int Account::getNbAccounts(void) {
+	return (Account::_nbAccounts);
+}
+int Account::getTotalAmount(void) {
+	return (Account::_totalAmount);
+}
+int Account::getNbDeposits(void) {
+	return (Account::_totalNbDeposits);
+}
+int Account::getNbWithdrawals(void) {
+	return (Account::_totalNbWithdrawals);
+}
+
+void Account::displayAccountsInfos(void) {
+
+}
+
+int Account::checkAmount (void) const {
 
 	return (this->_amount);
 }
 
-void t::_displayTimestamp(void) {
-	
+void Account::_displayTimestamp(void) {
+
 }
